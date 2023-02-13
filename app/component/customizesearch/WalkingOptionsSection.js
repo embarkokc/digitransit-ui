ae520c19c8b4177aed72b1fb693245260c5d10b0
Holger Bruch <hb@mfdz.de>
2023-02-13 11:46:24 +0100

    feat: settings-page (wip)

--- app/component/customizesearch/WalkingOptionsSection.js
+++ app/component/customizesearch/WalkingOptionsSection.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { intlShape } from 'react-intl';
+import { intlShape, FormattedMessage } from 'react-intl';
 import { saveRoutingSettings } from '../../action/SearchSettingsActions';
 
 import { addAnalyticsEvent } from '../../util/analyticsUtils';
@@ -24,56 +24,58 @@ const WalkingOptionsSection = (
     ),
 ) => (
   <div className="walk-options-container">
-    <SearchSettingsDropdown
-      currentSelection={currentSelection}
-      defaultValue={defaultSettings.walkSpeed}
-      onOptionSelected={value => {
-        executeAction(saveRoutingSettings, {
-          walkSpeed: value,
-        });
-        addAnalyticsEvent({
-          category: 'ItinerarySettings',
-          action: 'ChangeWalkingSpeed',
-          name: value,
-        });
-      }}
-      options={options}
-      labelText={intl.formatMessage({ id: 'walking-speed' })}
-      highlightDefaulValue
-      formatOptions
-      name="walkspeed"
-    />
-    <div className="toggle-container walk-option-inner">
-      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-      <label
-        htmlFor="settings-toggle-avoid-walking"
-        className="settings-header toggle-label"
-      >
-        <div className="toggle-label-text">
-          {intl.formatMessage({ id: 'avoid-walking' })}
-        </div>
-        <Toggle
-          id="settings-toggle-avoid-walking"
-          toggled={
-            currentSettings.walkReluctance === walkReluctanceOptions.least
-          }
-          onToggle={() => {
-            const avoid =
-              currentSettings.walkReluctance !== walkReluctanceOptions.least;
-            executeAction(saveRoutingSettings, {
-              walkReluctance: avoid
-                ? walkReluctanceOptions.least
-                : defaultSettings.walkReluctance,
-            });
-            addAnalyticsEvent({
-              category: 'ItinerarySettings',
-              action: 'ChangeAmountOfWalking',
-              name: avoid ? 'avoid' : 'default',
-            });
-          }}
-        />
-      </label>
-    </div>
+    <fieldset>
+      <legend className="walking-subheader settings-header">
+        <FormattedMessage id="walking-mode" defaultMessage="Walking" />
+      </legend>
+      <SearchSettingsDropdown
+        currentSelection={currentSelection}
+        defaultValue={defaultSettings.walkSpeed}
+        onOptionSelected={value => {
+          executeAction(saveRoutingSettings, {
+            walkSpeed: value,
+          });
+          addAnalyticsEvent({
+            category: 'ItinerarySettings',
+            action: 'ChangeWalkingSpeed',
+            name: value,
+          });
+        }}
+        options={options}
+        labelText={intl.formatMessage({ id: 'walking-speed' })}
+        highlightDefaulValue
+        formatOptions
+        name="walkspeed"
+      />
+      <div className="toggle-container walk-option-inner">
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="settings-toggle-avoid-walking" className="toggle-label">
+          <div className="toggle-label-text">
+            {intl.formatMessage({ id: 'avoid-walking' })}
+          </div>
+          <Toggle
+            id="settings-toggle-avoid-walking"
+            toggled={
+              currentSettings.walkReluctance === walkReluctanceOptions.least
+            }
+            onToggle={() => {
+              const avoid =
+                currentSettings.walkReluctance !== walkReluctanceOptions.least;
+              executeAction(saveRoutingSettings, {
+                walkReluctance: avoid
+                  ? walkReluctanceOptions.least
+                  : defaultSettings.walkReluctance,
+              });
+              addAnalyticsEvent({
+                category: 'ItinerarySettings',
+                action: 'ChangeAmountOfWalking',
+                name: avoid ? 'avoid' : 'default',
+              });
+            }}
+          />
+        </label>
+      </div>
+    </fieldset>
   </div>
 );
 

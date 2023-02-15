cbb8fdd1b4a7f8ebf4a63f340220e6ea5fcb51bf
Jannis R <mail@jannisr.de>
2023-02-15 22:02:04 +0100

    FavouriteBar: don't render any favourites

--- digitransit-component/packages/digitransit-component-favourite-bar/src/index.js
+++ digitransit-component/packages/digitransit-component-favourite-bar/src/index.js
@@ -2,13 +2,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import cx from 'classnames';
 import i18next from 'i18next';
-import differenceWith from 'lodash/differenceWith';
-import isEqual from 'lodash/isEqual';
-import isEmpty from 'lodash/isEmpty';
 import Shimmer from '@hsl-fi/shimmer';
-import SuggestionItem from '@digitransit-component/digitransit-component-suggestion-item';
 import Icon from '@digitransit-component/digitransit-component-icon';
-import { formatFavouritePlaceLabel } from '@digitransit-search-util/digitransit-search-util-uniq-by-label';
 import styles from './helpers/styles.scss';
 import translations from './helpers/translations';
 
@@ -30,16 +25,6 @@ const isKeyboardSelectionEvent = event => {
   event.preventDefault();
   return true;
 };
-
-/**
- * Utility to format location [name, address] pair.
- * @param {LocationProperties} favourite
- * @returns {Array.<string>}
- */
-const formatFavourite = favourite =>
-  favourite && (favourite.name || favourite.address)
-    ? formatFavouritePlaceLabel(favourite.name, favourite.address)
-    : [];
 
 const FavouriteLocation = ({
   className,
@@ -129,9 +114,9 @@ class FavouriteBar extends React.Component {
     /** Optional. Function for selected "Edit" from suggestions. */
     onEdit: PropTypes.func,
     /** Optional. Function for "Add home" button. */
-    onAddHome: PropTypes.func,
+    // onAddHome: PropTypes.func,
     /** Optional. Function for "Add work" button. */
-    onAddWork: PropTypes.func,
+    // onAddWork: PropTypes.func,
     /** Optional. Language, fi, en or sv. */
     lang: PropTypes.string,
     /** Optional. Whether to show loading animation, true or false. */
@@ -149,8 +134,8 @@ class FavouriteBar extends React.Component {
     onClickFavourite: () => ({}),
     onAddPlace: () => ({}),
     onEdit: () => ({}),
-    onAddHome: () => ({}),
-    onAddWork: () => ({}),
+    // onAddHome: () => ({}),
+    // onAddWork: () => ({}),
     lang: 'fi',
     isLoading: false,
     color: '#007ac9',
@@ -170,45 +155,9 @@ class FavouriteBar extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      listOpen: false,
-      firstFavourite: props.favourites[0] || null,
-      secondFavourite: props.favourites[1] || null,
-      favourites: props.favourites.slice(2, props.favourites.length),
-      timestamp: 0,
-    };
-    this.expandListRef = React.createRef();
-    this.suggestionListRef = React.createRef();
-    this.firstItemRef = React.createRef();
     Object.keys(translations).forEach(lang => {
       i18next.addResourceBundle(lang, 'translation', translations[lang]);
     });
-  }
-
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const { favourites } = prevState;
-    const nextFavourites = nextProps.favourites;
-    if (
-      !isEmpty(differenceWith(nextFavourites, favourites, isEqual)) ||
-      !isEmpty(differenceWith(favourites, nextFavourites, isEqual)) ||
-      !isEqual(nextFavourites, favourites) ||
-      isEmpty(nextFavourites)
-    ) {
-      return {
-        firstFavourite: nextFavourites[0] || null,
-        secondFavourite: nextFavourites[1] || null,
-        favourites: nextFavourites.slice(2, nextFavourites.length),
-      };
-    }
-    return null;
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
   }
 
   toggleList = () => {
@@ -280,42 +229,6 @@ class FavouriteBar extends React.Component {
     }
   };
 
-  renderSuggestion = (
-    item,
-    index,
-    ariaLabelSuffix = '',
-    className = undefined,
-  ) => {
-    const id = `favourite-suggestion-list--item-${index}`;
-    return (
-      <li>
-        <div
-          role="button"
-          type="button"
-          tabIndex="0"
-          key={`favourite-suggestion-item-${index}`}
-          id={id}
-          className={cx(styles['favourite-suggestion-item'])}
-          onClick={() => this.suggestionSelected(index)}
-          onKeyDown={e => this.handleKeyDown(e, index)}
-          ref={index === 0 ? this.firstItemRef : ''}
-          // eslint-disable-next-line jsx-a11y/no-autofocus
-          autoFocus={index === 0}
-          aria-label={`${item?.name || ''} ${
-            item?.address || ''
-          } ${ariaLabelSuffix}`}
-        >
-          <SuggestionItem
-            item={item}
-            iconColor={this.props.color}
-            className={className}
-            fontWeights={this.props.fontWeights}
-          />
-        </div>
-      </li>
-    );
-  };
-
   getCustomSuggestions = () => {
     const customSuggestions = [
       {
@@ -337,122 +250,15 @@ class FavouriteBar extends React.Component {
   };
 
   render() {
-    const { onClickFavourite, isLoading, fontWeights } = this.props;
-    const {
-      listOpen,
-      favourites,
-      firstFavourite,
-      secondFavourite,
-    } = this.state;
-    const expandIcon = this.props.favourites.length === 0 ? 'plus' : 'arrow';
+    const { fontWeights } = this.props;
 
     if (i18next.language !== this.props.lang) {
       i18next.changeLanguage(this.props.lang);
     }
 
-    const [name1, address1] = formatFavourite(firstFavourite);
-    const [name2, address2] = formatFavourite(secondFavourite);
-
     return (
       <div style={{ '--font-weight-medium': fontWeights.medium }}>
         <div className={styles['favourite-container']}>
-          <FavouriteLocation
-            text={name1 || i18next.t('add-home')}
-            label={address1 || ''}
-            clickItem={() =>
-              firstFavourite
-                ? onClickFavourite(firstFavourite)
-                : this.props.onAddHome()
-            }
-            iconId={
-              firstFavourite && firstFavourite.selectedIconId
-                ? FavouriteBar.FavouriteIconIdToNameMap[
-                    firstFavourite.selectedIconId
-                  ]
-                : 'home'
-            }
-            isLoading={isLoading}
-            color={this.props.color}
-          />
-          <FavouriteLocation
-            text={name2 || i18next.t('add-work')}
-            label={address2 || ''}
-            clickItem={() =>
-              secondFavourite
-                ? onClickFavourite(secondFavourite)
-                : this.props.onAddWork()
-            }
-            iconId={
-              secondFavourite && secondFavourite.selectedIconId
-                ? FavouriteBar.FavouriteIconIdToNameMap[
-                    secondFavourite.selectedIconId
-                  ]
-                : 'work'
-            }
-            isLoading={isLoading}
-            color={this.props.color}
-          />
-          {/* eslint-disable jsx-a11y/role-supports-aria-props */}
-          <button
-            type="button"
-            className={cx(styles.expandButton, styles[expandIcon], {
-              [styles.rotate]: listOpen,
-            })}
-            ref={this.expandListRef}
-            id="favourite-expand-button"
-            onKeyDown={e => this.handleKeyDown(e)}
-            onClick={() => this.toggleList()}
-            aria-haspopup
-            aria-pressed={this.state.listOpen}
-            aria-label={i18next.t('open-favourites')}
-          >
-            <Shimmer active={isLoading}>
-              <Icon img={expandIcon} color={this.props.color} />
-            </Shimmer>
-          </button>
-          {/* eslint-enable jsx-a11y/role-supports-aria-props */}
-        </div>
-        <div className={styles['favourite-suggestion-container']}>
-          {listOpen && (
-            <ul
-              className={styles['favourite-suggestion-list']}
-              id="favourite-suggestion-list"
-              ref={this.suggestionListRef}
-              aria-label={i18next.t('favourites-list')}
-            >
-              {favourites.map((item, index) => {
-                const favouriteLabel = formatFavouritePlaceLabel(
-                  item.name,
-                  item.address,
-                );
-                const address = favouriteLabel[1];
-
-                return this.renderSuggestion(
-                  {
-                    ...item,
-                    address,
-                    iconColor: this.props.color,
-                  },
-                  index,
-                  `, ${i18next.t('add-destination')}`,
-                );
-              })}
-              {favourites.length > 0 && (
-                <div aria-hidden className={styles.divider} />
-              )}
-              {this.getCustomSuggestions().map((item, index) =>
-                this.renderSuggestion(
-                  {
-                    ...item,
-                    iconColor: this.props.color,
-                  },
-                  favourites.length + index,
-                  undefined,
-                  'favouriteCustom',
-                ),
-              )}
-            </ul>
-          )}
         </div>
       </div>
     );

7413e066d9e359e818fbf0869ae68051cd0e64ba
Jannis R <mail@jannisr.de>
2023-05-30 16:56:06 +0200

    chore: SelectedStopPopupContent: reformat

--- app/component/SelectedStopPopupContent.js
+++ app/component/SelectedStopPopupContent.js
@@ -1,22 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const SelectedStopPopupContent = ({ stop }) => (
-  <div className="origin-popup">
-    <div className="origin-popup-header">
-      <div className="selected-stop-header">Stop {stop.code}</div>
-    </div>
-    <div>
-      <div className="origin-popup-name">
-        <div className="selected-stop-popup">
-          <p className="card-code">{stop.name}</p>
-          {stop.desc && <span className="description">{stop.desc}</span>}
-        </div>
+const SelectedStopPopupContent = ({ stop }) => {
+  return (
+    <div className="origin-popup">
+      <div className="origin-popup-header">
+        <div className="selected-stop-header">Stop {stop.code}</div>
       </div>
-      <div className="shade-to-white" />
+      <div>
+        <div className="origin-popup-name">
+          <div className="selected-stop-popup">
+            <p className="card-code">{stop.name}</p>
+            {stop.desc && <span className="description">{stop.desc}</span>}
+          </div>
+        </div>
+        <div className="shade-to-white" />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 SelectedStopPopupContent.propTypes = {
   stop: PropTypes.object.isRequired,

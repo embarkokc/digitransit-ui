497cc21e2d4ef8afe16e030a088b6399a3212e15
Holger Bruch <hb@mfdz.de>
2023-05-30 07:24:51 +0200

    feat: add route pdf download

--- app/configurations/config.okc.js
+++ app/configurations/config.okc.js
@@ -52,7 +52,7 @@ export default configMerger(walttiConfig, {
     PELIAS_REVERSE_GEOCODER: `${GEOCODING_BASE_URL}/reverse`,
     PELIAS_PLACE: `${GEOCODING_BASE_URL}/place`,
     FONT: null,
-    
+    ROUTE_PDF: ROOTLINK+'/assets/documents/Maps-Schedules/Route-{routeShortName}-Schedule-Map.pdf',
   },
 
   colors: {

3c79471578f2aec7eb9dea9849dd7754294b4a86
Jannis R <mail@jannisr.de>
2023-06-12 16:12:39 +0200

    fix: stops near you page: re-fetch results when searching for a `Location`

--- app/component/StopsNearYouSearch.js
+++ app/component/StopsNearYouSearch.js
@@ -10,9 +10,16 @@ import { getStopRoutePath } from '../util/path';
 const DTAutoSuggestWithSearchContext = withSearchContext(DTAutoSuggest);
 const searchSources = ['Favourite', 'History', 'Datasource'];
 
-function StopsNearYouSearch({ mode, breakpoint, lang }, { router, config }) {
+function StopsNearYouSearch(
+  { mode, breakpoint, lang, showNearbyStops },
+  { router, config },
+) {
   const isMobile = breakpoint !== 'large';
   const transportMode = `route-${mode}`;
+
+  const redirectToStopOrRoute = item => {
+    router.push(getStopRoutePath(item));
+  };
 
   const filter = config.stopSearchFilter
     ? (results, transportmode, type) =>
@@ -20,15 +27,27 @@ function StopsNearYouSearch({ mode, breakpoint, lang }, { router, config }) {
           config.stopSearchFilter,
         )
     : filterSearchResultsByMode;
-  const selectHandler = item => {
-    router.push(getStopRoutePath(item));
-  };
+
+  // Upstream (HSL), with `mode === 'CITYBIKE', this component only allows searching for citybike stations; It *does not* allow you to search for an address (`targets: 'Locations'`), so that the page would show nearby cityike stations.
+  // This component's `selectHandler` would always assume that a) it has been passed a stop or citybike station (depending on `mode`), and b) that it will redirect (`router.push()`) to an entirely different page.
+  // For Embark, with `mode === 'CITYBIKE'`, we have adding the `Locations` target to allow searching for an address.
+  // Thus, with `mode === 'CITYBIKE'`, the `selectHandler` now has to re-center the map and re-fetch nearby citybike stations; We have chosen to let the parent component pass this `selectHandler` implementation in (prop `showNearbyStops`).
+  // Because the `StopsNearYouPage`'s `selectHandler` implementation depends on a specific "flattened" data format, and because `withSearchContext()` only post-processes the selected item into this format if we pass `id: 'origin-stop-near-you'`, we need to set `id` accordingly.
+  let id = 'stop-route-station';
+  let targets = ['Stops', 'Routes'];
+  let selectHandler = redirectToStopOrRoute;
+  if (mode === 'CITYBIKE') {
+    id = 'origin-stop-near-you';
+    targets = ['BikeRentalStations', 'Locations'];
+    selectHandler = showNearbyStops;
+  }
+
   return (
     <div className="stops-near-you-search-container">
       <div className="search-container-first">
         <DTAutoSuggestWithSearchContext
           icon="search"
-          id="stop-route-station"
+          id={id}
           lang={lang}
           refPoint={origin}
           className="destination"
@@ -38,11 +57,7 @@ function StopsNearYouSearch({ mode, breakpoint, lang }, { router, config }) {
           value=""
           filterResults={filter}
           sources={searchSources}
-          targets={
-            mode === 'CITYBIKE'
-              ? ['BikeRentalStations', 'Locations']
-              : ['Stops', 'Routes']
-          }
+          targets={targets}
           isMobile={isMobile}
           selectHandler={selectHandler} // prop for context handler
           getAutoSuggestIcons={config.getAutoSuggestIcons}
@@ -58,6 +73,7 @@ StopsNearYouSearch.propTypes = {
   mode: PropTypes.string.isRequired,
   breakpoint: PropTypes.string.isRequired,
   lang: PropTypes.string.isRequired,
+  showNearbyStops: PropTypes.func.isRequired,
 };
 
 StopsNearYouSearch.contextTypes = {

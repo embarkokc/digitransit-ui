98511cc49874031f17b0d93238bfef04d27a4d5a
Holger Bruch <hb@mfdz.de>
2025-02-16 08:04:46 +0100

    fix: show 0 cents as free

--- app/component/TicketInformation.okc.js
+++ app/component/TicketInformation.okc.js
@@ -5,7 +5,7 @@ import Icon from './Icon';
 import { FareShape } from '../util/shapes';
 
 export default function TicketInformation({ fares }, { intl }) {
-  if (!fares || fares.length !== 1 || !fares[0].cents) {
+  if (!fares || fares.length !== 1 || typeof fares[0].cents !== 'number') {
     // FOR EMBARK, we assume that exactly one fare is returned.
     // Should there be more than one, we better show nothing, than
     // an unexpected partial fare
@@ -16,13 +16,19 @@ export default function TicketInformation({ fares }, { intl }) {
   return (
     <span className="okc-icon-button fare--itinerary-summary">
       <Icon img="icon-icon_ticket" />
-      <span>
-        {intl.formatNumber(fares[0].cents / 100, {
-          style: 'currency',
-          currency: 'USD',
-        })}
-      </span>
-      <a href={fareUrl}>Buy</a>
+      {fares[0].cents > 0 ? (
+        <>
+          <span>
+            {intl.formatNumber(fares[0].cents / 100, {
+              style: 'currency',
+              currency: 'USD',
+            })}
+          </span>
+          {fares[0].cents > 0 && fareUrl && <a href={fareUrl}>Buy</a>}
+        </>
+      ) : (
+        <span>free</span>
+      )}
     </span>
   );
 }
